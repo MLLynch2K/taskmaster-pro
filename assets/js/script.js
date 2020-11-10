8d05dc929e1a3bdd7a798f969f989fc18f362783
Michele Lee Lynch <michele.lynch8@yahoo.com>
2020-11-10 14:45:29 -0500

    updated task editing

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,8 +32,7 @@ var loadTasks = function() {
   }
 
   // loop over object properties
-  $.each(tasks, function(list, arr) {
-    console.log(list, arr);
+  $.each(tasks, function(list, arr) {;
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -81,6 +80,55 @@ $("#task-form-modal .btn-primary").click(function() {
     saveTasks();
   }
 });
+
+// save tasks function
+$(".list-group").on("click", "p", function(){
+  var text = $(this)
+    .text()
+    .trim();
+  console.log(text);
+
+  //create new input element
+  var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+
+  // swap out elements
+  $(this).replaceWith(textInput);
+  //focus on new elements
+  textInput.trigger("focus");
+});
+
+$(".list-group").on("blur", "textarea", function(){
+  // get the textarea's current value/text
+  var text = $(this)
+    .val()
+    .trip();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+tasks[status][index].text = text;
+saveTasks();
+
+//recreate p element
+var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+
+  //replace textarea with p element
+$(this).replaceWith(taskP);
+
+});
+
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
